66e280950e18f477f3ba59400222f75d822e96ac
Boity <boitumelomachimane@gmail.com>
2026-07-11 14:50:23 +0200

    Remove Spring Boot backend, migrating to Supabase

--- backend/src/main/java/com/grade12/backend/dto/LoginRequest.java
+++
@@ -1,9 +0,0 @@
-package com.grade12.backend.dto;
-
-import lombok.Data;
-
-@Data
-public class LoginRequest {
-    private String email;
-    private String password;
-}
--- backend/src/main/java/com/grade12/backend/model/LoginRequest.java
+++
@@ -1,9 +0,0 @@
-package com.grade12.backend.model;
-
-import lombok.Data;
-
-@Data
-public class LoginRequest {
-    private String email;
-    private String password;
-}
